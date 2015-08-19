4e68a2a63b988d106eaf94397b0330c8b9883644
Henry Miskin <henry.miskin@talktalk.net>
2015-08-19 16:18:27 +0100

    Resource demo cmpeted

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # SamProject
 
+##Install
+
+```
 virtualenv venv
 source venv/bin/activate
-
 pip install -r requirement.txt --allow-all-external
-python app.py
+```
+
+##Run
+
+```
+sudo python app.py
+0.0.0.0/8000
+```
--- client/app.js
+++ client/app.js
@@ -10,5 +10,18 @@ app.controller('NeuralController',['$scope','Result',NeuralController]);
 
 function NeuralController($scope,Result){
     $scope.result = Result.get({resultId:'id'});
+    
+    $scope.results = Result.query().$promise.then(function(){
+        console.log('loaded');
+    },function(error){
+        console.log(error);
+    })
 
+    var result = new Result();
+    result.name = 'hello';
+    result.$save().then(function(){
+        console.log('saved');
+    },function(error){
+        console.log(error);
+    })
 };
